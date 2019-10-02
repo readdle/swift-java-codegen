3f15fa3dd2bd48584758e934637984daea40ca63
Andrew Druk <adruk@readdle.com>
2019-10-02 22:15:35 +0300

    Dev: migrate SwiftEnvironment

---
+++ sample/src/main/java/com/readdle/swiftjava/sample/SwiftEnvironment.kt
@@ -0,0 +1,22 @@
+package com.readdle.swiftjava.sample
+
+import com.readdle.codegen.anotation.SwiftReference
+
+// Swift JNI private constructor
+// Should be private. Don't call this constructor from Java!
+@SwiftReference
+class SwiftEnvironment private constructor() {
+
+    // Swift JNI private native pointer
+    private val nativePointer = 0L
+
+    // Swift JNI release method
+    external fun release()
+
+    companion object {
+
+        @JvmStatic
+        external fun initEnvironment()
+    }
+
+}
